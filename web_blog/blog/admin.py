42bcc9deda4478cb56a34981fd1d2795af37c779
Jorge Alviarez <jjafblin@gmail.com>
2021-01-24 17:25:17 -0500

    blog models

--- web_blog/blog/admin.py
+++ web_blog/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Post
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields= ('created','modified')
+
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields= ('created','modified')
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Category,CategoryAdmin)
