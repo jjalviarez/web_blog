9a6f7cc09659016d658a2f11ad6010d620cacc5d
Jorge Alviarez <jjafblin@gmail.com>
2021-01-26 21:11:50 -0500

    add view and template to blog

--- web_blog/blog/urls.py
+++ web_blog/blog/urls.py
@@ -5,4 +5,5 @@ from web_blog.blog import views
 app_name = "blog"
 urlpatterns = [
         path('', views.blog, name='blog'),
+        path('category/<category_id>', views.category, name='category'),
 ]
