2376102f987a9a5b34aad9e68727d4c49420cba5
Jorge Alviarez <jjafblin@gmail.com>
2021-02-23 23:30:29 -0500

    admin and template store app

--- web_blog/store/admin.py
+++ web_blog/store/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-#from .models import Store
+from .models import Category, Product
 
 # Register your models here.
-#class StoreAdmin(admin.ModelAdmin):
-#    readonly_fields= ('created','modified')
 
-#admin.site.register(Store,StoreAdmin)
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields= ('created','modified')
+
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields= ('created','modified')
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Category,CategoryAdmin)
