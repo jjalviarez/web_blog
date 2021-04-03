041ecce61ad2a5f361217e9c79b77442801c1e60
Jorge Alviarez <jjafblin@gmail.com>
2021-04-03 12:34:58 -0500

    base app

--- config/settings/base.py
+++ config/settings/base.py
@@ -78,6 +78,7 @@ LOCAL_APPS = [
     "web_blog.blog.apps.BlogConfig",
     "web_blog.contact.apps.ContactConfig",
     "web_blog.store.apps.StoreConfig",
+    "web_blog.cart.apps.CartConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
---
+++ web_blog/cart/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CartConfig(AppConfig):
+    name = 'web_blog.cart'
+    verbose_name = ("Cart")
